63433dcf750777f27e7a99b75c5bb6d3ae397469
liss22 <liss22@hanmail.net>
2020-01-10 06:05:03 +0900

    Project: Cone 빈 페이지 추가됨

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import * as p5 from "p5";
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  notice: string[] = [];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewWillEnter() {
+    let sketch = (p5: p5) => {
+      p5.setup = () => {
+        p5.loadStrings('assets/data/notice.txt', v => {
+          this.notice = v;
+        })
+      }
+    }
+    new p5(sketch);
+  }
 }
