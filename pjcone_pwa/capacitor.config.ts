8bd3e3fed14f87b0462a44c0dbded5c8fb2d9fc4
liss22 <liss22@hanmail.net>
2022-05-06 06:02:25 +0900

    안드로이드 결과물 무시, AppId 설정됨

--- pjcone_pwa/capacitor.config.ts
+++ pjcone_pwa/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'pjcone_pwa',
+  appId: 'org.pjcone.portal',
+  appName: 'Project: Cone',
   webDir: 'www',
   bundledWebRuntime: false
 };
