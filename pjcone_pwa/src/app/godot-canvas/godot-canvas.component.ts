a3bf21f391d9d4d41bc12c53feb58d86ffc24f72
liss22 <liss22@hanmail.net>
2022-06-07 00:33:06 +0900

    고도 iframe으로 불러오는 컴포넌트 추가됨

---
+++ pjcone_pwa/src/app/godot-canvas/godot-canvas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-godot-canvas',
+  templateUrl: './godot-canvas.component.html',
+  styleUrls: ['./godot-canvas.component.scss'],
+})
+export class GodotCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
