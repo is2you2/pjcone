62a34fed71116c66e078eb0d8d87eec4eeb836f5
liss22 <liss22@hanmail.net>
2022-12-18 11:21:53 +0900

    1:1 채팅화면 진입시 점등 조정 오류 수정

--- src/app/portal/subscribes/chat-room/chat-room.page.ts
+++ src/app/portal/subscribes/chat-room/chat-room.page.ts
@@ -7,6 +7,7 @@ import { P5ToastService } from 'src/app/p5-toast.service';
 import * as p5 from "p5";
 import { ProfilePage } from '../../settings/profile/profile.page';
 import { OthersProfilePage } from 'src/app/others-profile/others-profile.page';
+import { StatusManageService } from 'src/app/status-manage.service';
 
 interface FileInfo {
   id?: string;
@@ -41,6 +42,7 @@ export class ChatRoomPage implements OnInit {
     public nakama: NakamaService,
     private noti: LocalNotiService,
     private p5toast: P5ToastService,
+    private statusBar: StatusManageService,
   ) { }
 
   /** 채널 정보 */
@@ -113,11 +115,13 @@ export class ChatRoomPage implements OnInit {
     this.noti.RemoveListener(`openchat${this.info['cnoti_id']}`);
     this.isOfficial = this.info['server']['isOfficial'];
     this.target = this.info['server']['target'];
+    console.log(this.info);
     // 1:1 대화라면
     if (this.info['redirect']['type'] == 2) {
       if (!this.info['redirect']) // 채널 최초 생성 오류 방지용
         this.info['status'] = this.info['info']['online'] ? 'online' : 'pending';
-      else this.info['status'] = this.nakama.load_other_user(this.info['redirect']['id'], this.isOfficial, this.target)['online'] ? 'online' : 'pending';
+      else if (this.statusBar.groupServer[this.isOfficial][this.target] == 'online')
+        this.info['status'] = this.nakama.load_other_user(this.info['redirect']['id'], this.isOfficial, this.target)['online'] ? 'online' : 'pending';
     }
     this.content_panel = document.getElementById('content');
     console.warn('이 자리에서 로컬 채팅 기록 불러오기');
