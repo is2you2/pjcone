8ad7fa2b11b0dcc39ad10473c3195cd587c962bf
liss22 <liss22@hanmail.net>
2023-01-31 15:04:54 +0900

    제작자 페이지: 번역 구성 추가, 제작자 이미지 및 정보 추가됨

--- src/app/portal/settings/creator/creator.page.ts
+++ src/app/portal/settings/creator/creator.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from "p5";
+import { LanguageSettingService } from 'src/app/language-setting.service';
 
 @Component({
   selector: 'app-creator',
@@ -7,18 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatorPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public lang: LanguageSettingService,
+  ) { }
 
   /** 번역가 페이지 정보 */
   info = {
-    /** 프로필 이미지 */
-    img: undefined,
     /** 번역가 표시명 */
     name: '정보 준비중',
     /** 긴 글 */
     text: '기능 준비중',
   };
 
-  ngOnInit() { }
+  ngOnInit() {
+    new p5((p: p5) => {
+      p.setup = () => {
+        p.loadJSON(`assets/data/infos/${this.lang.lang}/creator.json`, v => {
+          this.info = v;
+          p.remove();
+        }, e => {
+          console.error('번역가 정보 불러오기 실패: ', e);
+          p.remove();
+        });
+      }
+    });
+  }
 
 }
