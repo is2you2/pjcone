2ef3ae93dabd2487c3b05364478acdd7509cf65b
liss22 <liss22@hanmail.net>
2023-02-10 17:32:33 +0900

    Tool 다시받기 기능 추가됨

--- src/app/portal/settings/tool-management/tool-management.page.ts
+++ src/app/portal/settings/tool-management/tool-management.page.ts
@@ -3,8 +3,10 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { SERVER_PATH_ROOT } from 'src/app/app.component';
 import { IndexedDBService } from 'src/app/indexed-db.service';
 import { LanguageSettingService } from 'src/app/language-setting.service';
+import { P5ToastService } from 'src/app/p5-toast.service';
 
 @Component({
   selector: 'app-tool-management',
@@ -18,6 +20,7 @@ export class ToolManagementPage implements OnInit {
     private navParams: NavParams,
     private indexed: IndexedDBService,
     public lang: LanguageSettingService,
+    private p5toast: P5ToastService,
   ) { }
 
   list = [];
@@ -27,6 +30,32 @@ export class ToolManagementPage implements OnInit {
     data.forEach(path => {
       this.list.push(path.substring(5));
     });
+  }
+
+  /** 툴 다시 다운받기 */
+  async redownload_tool(i: number) {
+    try {
+      let res = await fetch(`${SERVER_PATH_ROOT}pjcone_pck/${this.list[i]}`);
+      if (res.ok) { // 다운로드에 성공한다면
+        let blob = await res.blob();
+        let reader: any = new FileReader();
+        reader = reader._realReader ?? reader;
+        reader.onload = (ev: any) => {
+          this.indexed.saveFileToUserPath(ev.target.result.replace(/"|\\|=/g, ''), `acts/${this.list[i]}`, () => {
+            this.p5toast.show({
+              text: `${this.lang.text['ToolManager']['redownloadSucc']}: ${this.list[i]}`,
+              lateable: true,
+            });
+          });
+        };
+        reader.readAsDataURL(blob);
+      } else throw new Error("없는거나 다름없지");
+    } catch (e) { // 로컬 정보 기반으로 광고
+      this.p5toast.show({
+        text: `${this.lang.text['ToolManager']['redownloadFailed']}: ${this.list[i]}`,
+        lateable: true,
+      });
+    }
   }
 
   /** 툴을 기기에서 삭제 */
