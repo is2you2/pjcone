4756c53eb39bb09cf019cceae5444965557dff5a
liss22 <liss22@hanmail.net>
2023-02-19 22:44:35 +0900

    Viewer-init 노드 수정됨 (Main.tscn -> TodoViewer.tscn)

--- src/app/portal/main/main.page.ts
+++ src/app/portal/main/main.page.ts
@@ -58,9 +58,6 @@ export class MainPage implements OnInit {
     this.app.CreateGodotIFrame('godot-todo', {
       act: 'godot-todo',
       title: 'Todo',
-      // 아래 주석 처리된 key들은 고도쪽에서 추가됨
-      // add_todo: 새 해야할 일 등록
-      // remove_todo: 해야할 일 삭제
       /**
        * 해야할 일 추가/수정/열람 메뉴 띄우기
        * @param _data 해당 해야할 일 정보
@@ -74,6 +71,9 @@ export class MainPage implements OnInit {
           },
         }).then(v => v.present());
       }
+      // 아래 주석 처리된 key들은 고도쪽에서 추가됨
+      // add_todo: 새 해야할 일 등록
+      // remove_todo: 해야할 일 삭제
     });
   }
 
