2d4d8a818619611f91ba4d3ffa55a5a6f73ca4b2
liss22 <liss22@hanmail.net>
2023-02-21 05:46:45 +0900

    @angular/pwa@13 추가됨 (설정 변경 없는 설치 기본값)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,18 @@ import { Device } from '@awesome-cordova-plugins/device/ngx';
 import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
 import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
 import { BarcodeScanner } from "@awesome-cordova-plugins/barcode-scanner/ngx";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Device,
     BackgroundMode,
