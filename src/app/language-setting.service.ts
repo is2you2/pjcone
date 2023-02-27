e58af9510730490ba3223f60395c1d660641da16
liss22 <liss22@hanmail.net>
2023-02-27 09:30:58 +0900

    언어가 완전히 로드된 후에 하단 메뉴 사용 가능

--- src/app/language-setting.service.ts
+++ src/app/language-setting.service.ts
@@ -74,6 +74,7 @@ export class LanguageSettingService {
   /** nakama 스크립트 상호참조를 우회하여 번역처리 */
   Callback_nakama: Function;
   Callback_WscClient: Function;
+  OnLoading = true;
   /** 순차적으로 번역처리하기 */
   ASyncTranslation(v: p5.Table, i: number, j: number, tmpTitle?: string) {
     if (i < j) {
@@ -88,6 +89,7 @@ export class LanguageSettingService {
     } else { // 전부 불러온 후
       this.Callback_nakama(this.text['GroupServer']['DevTestServer']);
       this.Callback_WscClient();
+      this.OnLoading = false;
     }
   }
 }
