87743117fb1928e9772c7729db66f359a547511b
liss22 <liss22@hanmail.net>
2023-05-28 22:16:31 +0900

    설정에서 오프라인 그룹 숨기기 추가

--- src/app/portal/subscribes/chat-room/void-draw/void-draw.page.ts
+++ src/app/portal/subscribes/chat-room/void-draw/void-draw.page.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2023 그림또따 <is2you246@gmail.com>
+// SPDX-License-Identifier: MIT
+
 import { Component, OnInit } from '@angular/core';
 import { AlertController, LoadingController, ModalController } from '@ionic/angular';
 import { GlobalActService } from 'src/app/global-act.service';
