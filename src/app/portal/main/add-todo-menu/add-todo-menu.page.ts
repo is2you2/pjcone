def36ccfbdaaa5c1d2819f2edfe66e361517f089
liss22 <liss22@hanmail.net>
2023-06-09 21:22:46 +0900

    첨부 파일이 전부 삭제된 경우 모든 첨부파일 및 썸네일 삭제

--- src/app/portal/main/add-todo-menu/add-todo-menu.page.ts
+++ src/app/portal/main/add-todo-menu/add-todo-menu.page.ts
@@ -817,6 +817,12 @@ export class AddTodoMenuPage implements OnInit {
         }
       });
       loading.dismiss();
+    } else { // 첨부된게 없다면 모든 이미지 삭제
+      if (received_json) { // 진입시 받은 정보가 있다면 수정 전 내용임
+        await this.indexed.removeFileFromUserPath(`todo/${this.userInput.id}/thumbnail.png`);
+        for (let i = 0, j = received_json.attach.length; i < j; i++)
+          await this.indexed.removeFileFromUserPath(received_json.attach[i]['path']);
+      }
     }
     this.userInput.attach.forEach(attach => {
       URL.revokeObjectURL(attach['img']);
