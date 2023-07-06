4f262fdf325a32ef7860e12e4fd82e2b5cab0fd2
liss22 <liss22@hanmail.net>
2023-07-06 15:32:53 +0900

    채널 인원 카운팅에 대해 try-catch 실행, 실패시 offline 표기

--- src/app/nakama.service.ts
+++ src/app/nakama.service.ts
@@ -1444,34 +1444,41 @@ export class NakamaService {
   /** 채널 상태 검토 */
   count_channel_online_member(p: any, _is_official: string, _target: string) {
     let result_status = 'pending';
-    if (p['group_id']) { // 그룹 채널인 경우
-      if (this.groups[_is_official][_target][p['group_id']] && this.groups[_is_official][_target][p['group_id']]['users']) {
-        let user_length = this.groups[_is_official][_target][p['group_id']]['users'].length;
-        if (user_length == 1) result_status = this.users.self['online'] ? 'online' : 'offline'; // 그룹에 혼자만 있음
-        else for (let i = 0; i < user_length; i++) { // 2명 이상의 그룹원
-          let userId = this.groups[_is_official][_target][p['group_id']]['users'][i]['user']['id'] || this.servers[_is_official][_target].session.user_id;
-          if (userId != this.servers[_is_official][_target].session.user_id) // 다른 사람인 경우
-            if (this.load_other_user(userId, _is_official, _target)['online']) {
-              result_status = 'online';
-              break;
-            }
+    try {
+      if (p['group_id']) { // 그룹 채널인 경우
+        if (this.groups[_is_official][_target][p['group_id']]
+          && this.groups[_is_official][_target][p['group_id']]['users']) {
+          this.servers[_is_official][_target].client.listGroupUsers
+          let user_length = this.groups[_is_official][_target][p['group_id']]['users'].length;
+          if (user_length == 1) result_status = this.users.self['online'] ? 'online' : 'offline'; // 그룹에 혼자만 있음
+          else for (let i = 0; i < user_length; i++) { // 2명 이상의 그룹원
+            let userId = this.groups[_is_official][_target][p['group_id']]['users'][i]['user']['id'] || this.servers[_is_official][_target].session.user_id;
+            if (userId != this.servers[_is_official][_target].session.user_id) // 다른 사람인 경우
+              if (this.load_other_user(userId, _is_official, _target)['online']) {
+                result_status = 'online';
+                break;
+              }
+          }
+        }
+      } else if (p['user_id_one']) { // 1:1 채팅인 경우
+        // 보통 내가 접근하면서 사용자 온라인 여부 검토를 할 때 채널이 없는 경우 오류가 남, 검토 후 채널 생성 처리
+        if (!this.channels_orig[_is_official][_target][p.channel_id || p.id])
+          this.join_chat_with_modulation(
+            p['user_id_one'] != this.servers[_is_official][_target].session.user_id ? p['user_id_one'] : p['user_id_two'],
+            2, _is_official, _target, (c) => {
+              let targetId = this.channels_orig[_is_official][_target][c.id]['redirect']['id'];
+              result_status = this.load_other_user(targetId, _is_official, _target)['online'] ? 'online' : 'pending';
+            });
+        else {
+          let targetId = this.channels_orig[_is_official][_target][p.channel_id || p.id]['redirect']['id'];
+          result_status = this.load_other_user(targetId, _is_official, _target)['online'] ? 'online' : 'pending';
         }
       }
-    } else if (p['user_id_one']) { // 1:1 채팅인 경우
-      // 보통 내가 접근하면서 사용자 온라인 여부 검토를 할 때 채널이 없는 경우 오류가 남, 검토 후 채널 생성 처리
-      if (!this.channels_orig[_is_official][_target][p.channel_id || p.id])
-        this.join_chat_with_modulation(
-          p['user_id_one'] != this.servers[_is_official][_target].session.user_id ? p['user_id_one'] : p['user_id_two'],
-          2, _is_official, _target, (c) => {
-            let targetId = this.channels_orig[_is_official][_target][c.id]['redirect']['id'];
-            result_status = this.load_other_user(targetId, _is_official, _target)['online'] ? 'online' : 'pending';
-          });
-      else {
-        let targetId = this.channels_orig[_is_official][_target][p.channel_id || p.id]['redirect']['id'];
-        result_status = this.load_other_user(targetId, _is_official, _target)['online'] ? 'online' : 'pending';
-      }
+    } catch (e) {
+      result_status = this.statusBar.groupServer[_is_official][_target] == 'offline' ? 'offline' : 'missing';
     }
-    this.channels_orig[_is_official][_target][p.channel_id || p.id]['status'] = result_status;
+    if (this.channels_orig[_is_official][_target][p.channel_id || p.id]['status'] != 'missing')
+      this.channels_orig[_is_official][_target][p.channel_id || p.id]['status'] = result_status;
   }
 
   check_if_online() {
