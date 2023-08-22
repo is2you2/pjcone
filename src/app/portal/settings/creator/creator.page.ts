723dae5528a6ec88b29f03215b5ee78c157ace6e
liss22 <liss22@hanmail.net>
2023-08-22 16:32:34 +0900

    제작자 이미지를 눌러 홈페이지로 이동

--- src/app/portal/settings/creator/creator.page.ts
+++ src/app/portal/settings/creator/creator.page.ts
@@ -54,6 +54,11 @@ export class CreatorPage implements OnInit {
     });
   }
 
+  /** 제작자 이미지를 눌러 홈페이지 이동 */
+  go_to_creator_page() {
+    window.open('https://is2you2.github.io/', '_system');
+  }
+
   /** 개발자에게 커피를 사주세요 */
   inAppPurchaseClicked() {
     console.log('커피 버튼');
