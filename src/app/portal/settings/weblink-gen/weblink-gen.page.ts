45fbf1fe0d2c9702abc30a795796637e17d4c14e
liss22 <liss22@hanmail.net>
2023-08-24 07:59:25 +0900

    기반 주소를 변경할 수 있음

--- src/app/portal/settings/weblink-gen/weblink-gen.page.ts
+++ src/app/portal/settings/weblink-gen/weblink-gen.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Clipboard } from '@awesome-cordova-plugins/clipboard/ngx';
+import clipboard from 'clipboardy';
 import { LanguageSettingService } from 'src/app/language-setting.service';
 
 @Component({
@@ -10,9 +12,45 @@ export class WeblinkGenPage implements OnInit {
 
   constructor(
     public lang: LanguageSettingService,
+    private mClipboard: Clipboard,
   ) { }
 
+  userInput = {
+    root: undefined,
+    open_profile: false,
+    open_subscribes: false,
+    use_tmp_user: false,
+    tmp_user: {
+      email: undefined,
+      password: undefined,
+      display_name: undefined,
+    },
+    servers: [],
+    groups: [],
+    group_dedi: undefined,
+  }
+
+  isSSLConnect = false;
+
   ngOnInit() {
+    this.isSSLConnect = window.location.protocol == 'https:';
+  }
+
+  baseURLChanged(ev: any) {
+    if (!ev.detail.value) {
+      this.userInput.root = undefined;
+    }
+    this.information_changed();
+  }
+
+  result_address = 'https://is2you2.github.io/pjcone_pwa/';
+  information_changed() {
+    this.result_address = this.userInput.root || 'https://is2you2.github.io/pjcone_pwa/';
+  }
+
+  copy_result_address() {
+    this.mClipboard.copy(this.result_address)
+      .catch(_e => clipboard.write(this.result_address));
   }
 
 }
