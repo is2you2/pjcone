c8a763a565156ce309de1feb7299c526cc211b9d
liss22 <liss22@hanmail.net>
2023-08-25 02:42:32 +0900

    이미지 뷰어 세로 짤림 문제 수정

--- src/app/portal/subscribes/chat-room/ionic-viewer/ionic-viewer.page.ts
+++ src/app/portal/subscribes/chat-room/ionic-viewer/ionic-viewer.page.ts
@@ -90,12 +90,19 @@ export class IonicViewerPage implements OnInit {
           }
           /** 미디어 플레이어 크기 및 캔버스 크기 조정 */
           let RePositioningImage = () => {
-            canvasDiv.style.backgroundSize = `${canvasDiv.clientWidth}px`;
-            canvasDiv.style.backgroundPositionX = '0px';
-            let imageRatio = canvasDiv.clientWidth / imageOriginalSize.x;
-            let centerHeight =
-              canvasDiv.clientHeight / 2 - imageOriginalSize.y * imageRatio / 2;
-            canvasDiv.style.backgroundPositionY = `${centerHeight}px`;
+            if (this.image_info['width'] > this.image_info['height']) {
+              canvasDiv.style.backgroundSize = `${canvasDiv.clientWidth}px`;
+              canvasDiv.style.backgroundPositionX = '0px';
+              let imageRatio = canvasDiv.clientWidth / imageOriginalSize.x;
+              let centerHeight =
+                canvasDiv.clientHeight / 2 - imageOriginalSize.y * imageRatio / 2;
+              canvasDiv.style.backgroundPositionY = `${centerHeight}px`;
+            } else {
+              let tmp_width = this.image_info['width'] * canvasDiv.clientHeight / this.image_info['height'];
+              canvasDiv.style.backgroundSize = `${tmp_width}px`;
+              canvasDiv.style.backgroundPositionX = `${(canvasDiv.clientWidth - tmp_width) / 2}px`;
+              canvasDiv.style.backgroundPositionY = `0px`;
+            }
           }
           p.windowResized = () => {
             setTimeout(() => {
