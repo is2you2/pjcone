4a6954e235955133b4fec55e576aa17e07891110
liss22 <liss22@hanmail.net>
2023-08-31 07:53:30 +0900

    QR코드 생성기 진입시 클립보드 내용으로 생성하기

--- src/app/portal/settings/qrcode-gen/qrcode-gen.page.ts
+++ src/app/portal/settings/qrcode-gen/qrcode-gen.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalActService } from 'src/app/global-act.service';
 import { LanguageSettingService } from 'src/app/language-setting.service';
+import clipboard from 'clipboardy';
+import { Clipboard } from '@awesome-cordova-plugins/clipboard/ngx';
+
 
 @Component({
   selector: 'app-qrcode-gen',
@@ -12,15 +15,25 @@ export class QrcodeGenPage implements OnInit {
   constructor(
     public lang: LanguageSettingService,
     private global: GlobalActService,
+    private mClipboard: Clipboard,
   ) { }
 
   /** 정보글 불러오기 */
   QRCode: any;
   userInput: string;
 
-  ngOnInit() { }
+  async ngOnInit() {
+    let paste: string;
+    try {
+      paste = await this.mClipboard.paste();
+    } catch (e) {
+      paste = await clipboard.read();
+    }
+    this.userInput = paste;
+    this.textarea_changed();
+  }
 
-  textarea_changed(_ev: any) {
+  textarea_changed(_ev?: any) {
     if (this.userInput)
       this.QRCode = this.global.readasQRCodeFromString(this.userInput);
     else this.QRCode = undefined;
