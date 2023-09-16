07904589f0421adfd378bde7b0b85ba3e3c5aba5
liss22 <liss22@hanmail.net>
2023-09-17 02:50:07 +0900

    채널 채팅 내 메시지의 시간이 다를 때마다 이름을 다시 표시

--- src/app/portal/subscribes/chat-room/chat-room.page.ts
+++ src/app/portal/subscribes/chat-room/chat-room.page.ts
@@ -909,13 +909,13 @@ export class ChatRoomPage implements OnInit, OnDestroy {
     this.messages[i]['showInfo']['sender'] = !this.messages[i].content.noti && this.messages[i].user_display_name;
     // 이전 메시지와 정보를 비교하여 이전 메시지와 지금 메시지의 상태를 결정 (실시간 메시지 받기류)
     if (i - 1 >= 0) {
-      this.messages[i]['showInfo']['date'] = Boolean(this.messages[i]['msgDate']) && (this.messages[i]['msgDate'] != this.messages[i - 1]['msgDate']);;
-      this.messages[i]['showInfo']['sender'] = !this.messages[i].content.noti && this.messages[i].user_display_name && (this.messages[i - 1]['isLastRead'] || this.messages[i].sender_id != this.messages[i - 1].sender_id || this.messages[i - 1].content.noti || this.messages[i]['msgDate'] != this.messages[i - 1]['msgDate']);
+      this.messages[i]['showInfo']['date'] = Boolean(this.messages[i]['msgDate']) && (this.messages[i]['msgDate'] != this.messages[i - 1]['msgDate']);
+      this.messages[i]['showInfo']['sender'] = !this.messages[i].content.noti && this.messages[i].user_display_name && (this.messages[i - 1]['isLastRead'] || this.messages[i].sender_id != this.messages[i - 1].sender_id || this.messages[i - 1].content.noti || this.messages[i]['msgDate'] != this.messages[i - 1]['msgDate'] || this.messages[i]['msgTime'] != this.messages[i - 1]['msgTime']);
     }
     // 다음 메시지와 정보를 비교하여 다음 메시지의 상태를 결정 (기록 불러오기류)
     if (i + 1 < j) {
       this.messages[i + 1]['showInfo']['date'] = Boolean(this.messages[i]['msgDate']) && (this.messages[i]['msgDate'] != this.messages[i + 1]['msgDate']);
-      this.messages[i + 1]['showInfo']['sender'] = !this.messages[i + 1].content.noti && this.messages[i + 1].user_display_name && (this.messages[i]['isLastRead'] || this.messages[i].sender_id != this.messages[i + 1].sender_id || this.messages[i].content.noti || this.messages[i]['msgDate'] != this.messages[i + 1]['msgDate']);
+      this.messages[i + 1]['showInfo']['sender'] = !this.messages[i + 1].content.noti && this.messages[i + 1].user_display_name && (this.messages[i]['isLastRead'] || this.messages[i].sender_id != this.messages[i + 1].sender_id || this.messages[i].content.noti || this.messages[i]['msgDate'] != this.messages[i + 1]['msgDate'] || this.messages[i]['msgTime'] != this.messages[i + 1]['msgTime']);
     }
     // url 링크 개체 즉시 불러오기
     if (this.messages[i]['content']['url'])
