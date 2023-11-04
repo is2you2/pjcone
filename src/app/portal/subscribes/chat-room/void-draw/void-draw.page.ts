3a3abe42e010df8102b4f73bab6563f908aa3fc7
liss22 <liss22@hanmail.net>
2023-11-04 20:45:12 +0900

    베이스 캔버스 구성 변경

--- src/app/portal/subscribes/chat-room/void-draw/void-draw.page.ts
+++ src/app/portal/subscribes/chat-room/void-draw/void-draw.page.ts
@@ -55,47 +55,44 @@ export class VoidDrawPage implements OnInit {
     }
     document.addEventListener('ionBackButton', this.EventListenerAct);
     this.mainLoading = await this.loadingCtrl.create({ message: this.lang.text['voidDraw']['UseThisImage'] });
-    this.create_p5voidDraw();
-    if (this.navParams.data['path'])
-      this.p5voidDraw['new_canvas']({
-        width: this.navParams.data.width,
-        height: this.navParams.data.height,
-        path: this.navParams.data.path,
-      });
-    else this.p5voidDraw['new_canvas']();
+    this.create_new_canvas({
+      width: this.navParams.data.width,
+      height: this.navParams.data.height,
+      path: this.navParams.data.path,
+    });
+  }
+
+  /** 새 캔버스 생성 행동 분리 */
+  create_new_canvas(inputInfo?: any) {
+    inputInfo['width'] = inputInfo['width'] || 432;
+    inputInfo['height'] = inputInfo['height'] || 432;
+    if (this.p5voidDraw) this.p5voidDraw.remove();
+    this.create_p5voidDraw(inputInfo);
   }
 
   p5voidDraw: p5;
-  create_p5voidDraw() {
+  create_p5voidDraw(initData: any) {
     let targetDiv = document.getElementById('voidDraw');
     this.p5voidDraw = new p5((p: p5) => {
-      /** 배경 이미지 */
+      /** 스케일 조정 편의를 위해 모든게 여기서 작업됨 */
+      let ActualCanvas: p5.Graphics;
+      /** 편집하려는 이미지 */
       let BaseImage: p5.Image;
       let canvas: p5.Renderer;
-      p.setup = () => {
-        let initData = { width: 432, height: 432 };
+      p.setup = async () => {
         p.pixelDensity(1);
         p.smooth();
-        p['new_canvas'] = async (data: any) => { // 첨부파일이 있으면 덮어씌우기
-          initData = { ...initData, ...data };
-          if (initData['path']) { // 배경 이미지 파일이 포함됨
-            let blob = await this.indexed.loadBlobFromUserPath(initData['path'], '');
-            let FileURL = URL.createObjectURL(blob);
-            p.loadImage(FileURL, v => {
-              BaseImage = v;
-              URL.revokeObjectURL(FileURL);
-            }, e => {
-              console.error('그림판 배경 이미지 불러오기 오류: ', e);
-              URL.revokeObjectURL(FileURL);
-            });
-          }
-          if (canvas) p.remove();
-          let targetSide = getRatioOfImageWindow(initData);
-          canvas = p.createCanvas(targetSide.x, targetSide.y);
-          canvas.parent(targetDiv);
-          // 화면 비율에 맞게 작업 해상도 낮추기
-          p['SetCanvasViewportInit']();
-        }
+        p.noLoop();
+        p.imageMode(p.CENTER);
+        // 정보 초기화
+        BaseImage = undefined;
+        if (ActualCanvas) ActualCanvas.remove();
+        ActualCanvas = undefined;
+        if (canvas) p.remove();
+        canvas = p.createCanvas(targetDiv.clientWidth, targetDiv.clientHeight);
+        RelativePosition.x = p.width / 2;
+        RelativePosition.y = p.height / 2;
+        canvas.parent(targetDiv);
         p['set_line_weight'] = () => {
           console.log('set_line_weight');
         }
@@ -112,33 +109,57 @@ export class VoidDrawPage implements OnInit {
 
         }
         p['SetCanvasViewportInit'] = () => {
-          let targetSide = getRatioOfImageWindow(initData);
-          p.resizeCanvas(targetSide.x, targetSide.y);
-          canvas.style('position', 'absolute');
-          canvas.style('top', '50%');
-          canvas.style('left', '50%');
-          canvas.style('transform', 'translateX(-50%) translateY(-50%)');
+          p.resizeCanvas(targetDiv.clientWidth, targetDiv.clientHeight);
+          RelativePosition.x = p.width / 2;
+          RelativePosition.y = p.height / 2;
+          let windowRatio = targetDiv.clientWidth / targetDiv.clientHeight;
+          let canvasRatio = ActualCanvas.width / ActualCanvas.height;
+          console.log(windowRatio, '/', canvasRatio, ' = ', windowRatio / canvasRatio);
+          RelativeScale = 1;
+          p.redraw();
+        }
+        ActualCanvas = p.createGraphics(initData.width, initData.height, p.WEBGL);
+        ActualCanvas.smooth();
+        ActualCanvas.noLoop();
+        // 사용자 그리기 판넬 생성
+        if (initData['path']) { // 배경 이미지 파일이 포함됨
+          let blob = await this.indexed.loadBlobFromUserPath(initData['path'], '');
+          let FileURL = URL.createObjectURL(blob);
+          p.loadImage(FileURL, v => {
+            ActualCanvas.resizeCanvas(v.width, v.height);
+            ActualCanvas.imageMode(p.CENTER);
+            BaseImage = v;
+            URL.revokeObjectURL(FileURL);
+            p['SetCanvasViewportInit']();
+            p.redraw();
+          }, e => {
+            console.error('그림판 배경 이미지 불러오기 오류: ', e);
+            URL.revokeObjectURL(FileURL);
+          });
+        } else {
+          p['SetCanvasViewportInit']();
           p.redraw();
         }
         this.initialized = true;
       }
+      /** Viewport 행동을 위한 변수들 */
+      let RelativePosition = p.createVector();
+      let RelativeScale = 1;
+      /** 이미지 Crop 시 상대적 위치 기록 */
+      let CropPosition = p.createVector();
       p.draw = () => {
-        if (!BaseImage) p.background(255);
-        else p.image(BaseImage, 0, 0, p.width, p.height);
-      }
-      let getRatioOfImageWindow = (initData: any): p5.Vector => {
-        let targetWidth = initData.width;
-        let targetHeight = initData.height;
-        let imageRatio = initData.width / initData.height;
-        let windowRatio = targetDiv.clientWidth / targetDiv.clientHeight;
-        if (imageRatio > windowRatio) { // 화면 가로에 이미지 맞추기
-          targetHeight = targetHeight / targetWidth * targetDiv.clientWidth;
-          targetWidth = targetDiv.clientWidth;
-        } else { // 화면 세로에 이미지 맞추기
-          targetWidth = targetWidth / targetHeight * targetDiv.clientHeight;
-          targetHeight = targetDiv.clientHeight;
-        }
-        return p.createVector(targetWidth, targetHeight);
+        p.clear(255, 255, 255, 255);
+        p.push();
+        p.translate(RelativePosition);
+        p.scale(RelativeScale);
+        ActualCanvas.background(255);
+        ActualCanvas.push();
+        ActualCanvas.translate(CropPosition);
+        if (BaseImage) ActualCanvas.image(BaseImage, 0, 0);
+        ActualCanvas.pop();
+        ActualCanvas.redraw();
+        p.image(ActualCanvas, 0, 0);
+        p.pop();
       }
       p.windowResized = () => {
         setTimeout(() => {
@@ -183,7 +204,7 @@ export class VoidDrawPage implements OnInit {
           else v.width = Number(v.width);
           if (!v.height) v.height = DEFAULT_SIZE;
           else v.height = Number(v.height);
-          this.p5voidDraw['new_canvas'](v);
+          this.create_new_canvas(v);
         }
       }],
     }).then(v => v.present());
@@ -218,6 +239,7 @@ export class VoidDrawPage implements OnInit {
   ionViewWillLeave() {
     delete this.global.p5key['KeyShortCut']['HistoryAct'];
     delete this.global.p5key['KeyShortCut']['AddAct'];
+    if (this.p5voidDraw) this.p5voidDraw.remove();
   }
 
   WithoutSave = true;
