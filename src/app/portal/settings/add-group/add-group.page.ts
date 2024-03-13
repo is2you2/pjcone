3f0eee2e65c4528b7ddf55aab998a783aeaf7e10
liss22 <liss22@hanmail.net>
2024-03-13 21:56:57 +0900

    채널 생성시 로컬 채널을 기본값으로

--- src/app/portal/settings/add-group/add-group.page.ts
+++ src/app/portal/settings/add-group/add-group.page.ts
@@ -43,7 +43,7 @@ export class AddGroupPage implements OnInit {
       target: 'channels',
       local: true,
     };
-    this.servers.push(local_info);
+    this.servers.unshift(local_info);
     this.userInput.server = this.servers[this.index];
     this.file_sel_id = `add_group_${new Date().getTime()}`;
   }
