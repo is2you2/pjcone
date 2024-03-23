15cbf9ff04083b6bb8d49532b30bfbfd3d654940
liss22 <liss22@hanmail.net>
2024-03-23 17:07:12 +0900

    테스터 분류 뺌, 로그아웃 글자색 변경

--- src/app/portal/settings/creator/creator.page.ts
+++ src/app/portal/settings/creator/creator.page.ts
@@ -25,7 +25,6 @@ export class CreatorPage implements OnInit {
     name: undefined,
   };
   special_thanks_to = [];
-  tester = [];
 
   isMobileApp = false;
 
@@ -63,18 +62,6 @@ export class CreatorPage implements OnInit {
           p.remove();
         }, e => {
           console.error('도움주신분들 정보 불러오기 실패: ', e);
-          p.remove();
-        });
-      }
-    });
-    new p5((p: p5) => {
-      p.setup = () => {
-        p.noCanvas();
-        p.loadJSON(`assets/data/infos/tester.json`, v => {
-          this.tester = v;
-          p.remove();
-        }, e => {
-          console.error('테스트 정보 불러오기 실패: ', e);
           p.remove();
         });
       }
