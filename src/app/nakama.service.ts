9becf993f4c482418bdaf0f1fd452fa17ec837de
liss22 <liss22@hanmail.net>
2024-04-07 13:59:59 +0900

    삭제된 채널의 상태를 손상처리함

--- src/app/nakama.service.ts
+++ src/app/nakama.service.ts
@@ -1525,6 +1525,8 @@ export class NakamaService {
                 if (this.groups[_is_official][_target] && this.groups[_is_official][_target][this.channels_orig[_is_official][_target][_cid]['redirect']['id']]) { // 유효한 그룹인 경우
                   this.channels_orig[_is_official][_target][_cid]['info'] = this.groups[_is_official][_target][this.channels_orig[_is_official][_target][_cid]['redirect']['id']];
                   this.channels_orig[_is_official][_target][_cid]['title'] = this.groups[_is_official][_target][this.channels_orig[_is_official][_target][_cid]['redirect']['id']]['name'];
+                  if (this.groups[_is_official][_target][this.channels_orig[_is_official][_target][_cid]['redirect']['id']]['status'] == 'missing')
+                    this.channels_orig[_is_official][_target][_cid]['status'] = this.groups[_is_official][_target][this.channels_orig[_is_official][_target][_cid]['redirect']['id']]['status'];
                 } else this.channels_orig[_is_official][_target][_cid]['status'] = 'missing';
               }
               break;
