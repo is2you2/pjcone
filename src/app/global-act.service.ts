b2fb886769bcfb8d799e4a7163e7748b6ec4a53e
liss22 <liss22@hanmail.net>
2024-04-23 20:01:36 +0900

    webm 확장자 별도 처리 (비디오/오디오 동시 구성 가능함)

--- src/app/global-act.service.ts
+++ src/app/global-act.service.ts
@@ -586,8 +586,14 @@ export class GlobalActService {
       // 비디오류
       case 'mp4':
       case 'ogv':
-      case 'webm':
         info['viewer'] = 'video';
+        break;
+      // 웹 미디어 병행가능한 구성
+      case 'webm':
+        if (info['type'].indexOf('audio/') == 0)
+          info['viewer'] = 'audio';
+        else if (info['type'].indexOf('video/') == 0)
+          info['viewer'] = 'video';
         break;
       // 코드류
       case 'cs': // C#
