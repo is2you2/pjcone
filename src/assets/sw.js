7640dd4d5f3ea5139f7218ce7b806155843419ac
liss22 <liss22@hanmail.net>
2024-05-25 20:07:36 +0900

    PWA Offline 에 맞춰 서비스 워커 변경 (테스트 되지 않음), 절대 경로를 쓰지 않았으므로 오류가 날 수 있음

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -1,5 +1,29 @@
-addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+    `${sub_path}`, `${sub_path}index.html`, `${sub_path}*.js`, `${sub_path}*.css`, `${sub_path}*.txt`, `${sub_path}svg/*``${sub_path}assets/*`
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
     );
 });
