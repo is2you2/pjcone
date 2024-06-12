1b756689918dc7be01b94409524dd0d0d2e7fe57
liss22 <liss22@hanmail.net>
2024-06-13 03:32:29 +0900

    설치 단계에서 앱 캐싱을 시도하므로 오프라인 동작은 제외시킴, 만약을 위해 주석으로 삭제처리

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -20,21 +20,21 @@ self.addEventListener('fetch', event => {
 });
 
 // 오프라인 상황에서의 동작 정의
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => {
-                return caches.match('/offline.html'); // 오프라인 메시지 페이지 반환
-            });
-        })
-    );
-});
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request).then(response => {
+//             return response || fetch(event.request).catch(() => {
+//                 return caches.match('/offline.html'); // 오프라인 메시지 페이지 반환
+//             });
+//         })
+//     );
+// });
 
 // Push 알림 처리
-self.addEventListener('push', event => {
-    const title = 'Push 알림';
-    const options = {
-        body: event.data.text()
-    };
-    event.waitUntil(self.registration.showNotification(title, options));
-});
+// self.addEventListener('push', event => {
+//     const title = 'Push 알림';
+//     const options = {
+//         body: event.data.text()
+//     };
+//     event.waitUntil(self.registration.showNotification(title, options));
+// });
