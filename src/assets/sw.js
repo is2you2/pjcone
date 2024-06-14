3ae96962ac82f67d77f7d0d573412cdb505f86c3
liss22 <liss22@hanmail.net>
2024-06-14 23:01:18 +0900

    좀 더 간결한 sw.js 코드 처리

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -28,24 +28,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          });
-      })
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
   );
 });
