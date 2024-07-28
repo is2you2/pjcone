5dacf0884a1e4499a65c68f8ca7ee2bd9e32cc7c
liss22 <liss22@hanmail.net>
2024-07-28 18:10:51 +0900

    서비스 워커 구성 변경

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -1,13 +1,14 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
+// 경로 업데이트 필수
+const URLs = [
+  '/index.html',
+]
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function (cache) {
-        // 경로 업데이트 필수
-        return cache.addAll([
-          '/index.html',
-        ]);
+      .then((cache) => {
+        return cache.addAll(URLs);
       })
   );
 });
@@ -28,8 +29,20 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(function () {
+    fetch(event.request).catch(() => {
       return caches.match(event.request);
     })
   );
+});
+
+// 오프라인 상태에서 동작 설정
+self.addEventListener('offline', (event) => {
+  console.log('You are now offline.');
+  // 오프라인 상태에 대한 UI/UX 처리 추가
+});
+
+// 온라인 상태에서 동작 설정
+self.addEventListener('online', (event) => {
+  console.log('You are now online.');
+  // 동기화 로직 실행 등 추가 작업
 });
