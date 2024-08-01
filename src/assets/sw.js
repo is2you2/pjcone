c0c98c5e2b20c6071a4a5a8c7a2a2d20a1fd41a9
liss22 <liss22@hanmail.net>
2024-08-01 23:30:20 +0900

    서비스 워커 내용 업데이트

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -1,21 +1,15 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
 // 경로 업데이트 필수
-// runtime.js 는 경로에서 무시하기
 const URLs = [
   '/index.html',
 ]
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return Promise.all(
-          URLs.map(url => {
-            cache.add(url);
-          })
-        );
-      })
-  );
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    for (let i = 0, j = URLs.length; i < j; i++)
+      await cache.add(URLs[i]);
+  })());
 });
 
 self.addEventListener('activate', event => {
@@ -32,10 +26,17 @@ self.addEventListener('activate', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  if (!(
+    e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+  )) return;
+
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(CACHE_NAME);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
