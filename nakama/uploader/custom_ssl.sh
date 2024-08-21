958bd5f01608c8030ceac6872cda96213031aa40
liss22 <liss22@hanmail.net>
2024-08-21 23:34:48 +0900

    사용된 req 파일 삭제하기, 자동 경로처리

--- nakama/uploader/custom_ssl.sh
+++ nakama/uploader/custom_ssl.sh
@@ -1,3 +1,4 @@
-openssl genpkey -algorithm RSA -out private.key
-openssl req -new -key private.key -out request.csr
-openssl req -x509 -nodes -days 365 -key private.key -in request.csr -out public.crt
+openssl genpkey -algorithm RSA -out ../data/private.key
+openssl req -new -key ../data/private.key -out request.csr
+openssl req -x509 -nodes -days 365 -key ../data/private.key -in request.csr -out ../data/public.crt
+rm request.csr
