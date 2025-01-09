9bd20963a40f883e24ee36436770e8e85596b728
liss22 <liss22@hanmail.net>
2025-01-10 03:37:46 +0900

    electron 출력 구성 추가됨

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 544,
+        height: 977,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // 메뉴를 비활성화
+    Menu.setApplicationMenu(null);
+
+    mainWindow.loadURL('https://is2you2.github.io/pjcone_pwa/');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+// Electron 앱이 준비되면 창을 생성
+app.on('ready', createWindow);
+
+// 모든 창이 닫히면 애플리케이션 종료
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
