91cd245c3be63ff58f0cb67de49c5667277db590
liss22 <liss22@hanmail.net>
2025-01-10 03:49:19 +0900

    다중창 관리, 비보안 연결처리

--- electron/main.js
+++ electron/main.js
@@ -2,14 +2,33 @@ const { app, BrowserWindow, Menu } = require('electron');
 
 let mainWindow;
 
-function createWindow() {
+app.on('ready', () => {
+    // 단일 인스턴스 보장
+    const gotTheLock = app.requestSingleInstanceLock();
+
+    if (!gotTheLock) {
+        app.quit(); // 이미 실행 중이면 종료
+        return;
+    }
+
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // 이미 실행 중인 애플리케이션에 새 인스턴스가 요청되었을 때 처리
+        if (mainWindow) {
+            // 창이 열린 상태라면 포커스를 주거나 동작을 정의
+            if (mainWindow.isMinimized()) mainWindow.restore();
+            mainWindow.focus();
+        }
+    });
+
+    // 메인 윈도우 생성
     mainWindow = new BrowserWindow({
         width: 450,
         height: 720,
         minWidth: 300, // 최소 너비
         minHeight: 300, // 최소 높이
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            webSecurity: false,
         }
     });
 
@@ -21,10 +40,7 @@ function createWindow() {
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
-}
-
-// Electron 앱이 준비되면 창을 생성
-app.on('ready', createWindow);
+});
 
 // 모든 창이 닫히면 애플리케이션 종료
 app.on('window-all-closed', () => {
