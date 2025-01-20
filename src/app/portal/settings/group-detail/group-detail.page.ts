59a07e20e21282ffc58f8e15d7b780e9ca3c3226
liss22 <liss22@hanmail.net>
2025-01-20 23:34:44 +0900

    페이지를 안볼 때 그룹 채널 이미지 변경에 오류가 발생하는 것을 완화

--- src/app/portal/settings/group-detail/group-detail.page.ts
+++ src/app/portal/settings/group-detail/group-detail.page.ts
@@ -315,7 +315,7 @@ export class GroupDetailPage implements OnInit, OnDestroy {
       this.announce_update_group_image(this.info.img);
     });
     let input = document.getElementById(this.file_sel_id) as HTMLInputElement;
-    input.value = '';
+    if (input) input.value = '';
   }
 
   announce_update_group_image(uri: string) {
